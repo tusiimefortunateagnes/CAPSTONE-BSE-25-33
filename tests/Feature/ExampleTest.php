3e2b5a77ed2cb7daa155d28e8a1e05ef18278594
Kimuli Arnold <kimarnold.dev@gmail.com>
2024-10-21 15:54:39 +0300

    build

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,22 +2,44 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-// use Tests\TestCase;
 use PHPUnit\Framework\TestCase;
-
 
 class ExampleTest extends TestCase
 {
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_that_true_is_true(): void
     {
         // $this->withoutExceptionHandling();
 
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->assertTrue(true);
     }
 }
+
+
+
+
+
+
+// namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Tests\TestCase;
+// use PHPUnit\Framework\TestCase;
+
+
+// class ExampleTest extends TestCase
+// {
+    /**
+     * A basic test example.
+     */
+    // public function test_the_application_returns_a_successful_response(): void
+    // {
+        // $this->withoutExceptionHandling();
+
+        // $response = $this->get('/');
+
+        // $response->assertStatus(200);
+    // }
+
